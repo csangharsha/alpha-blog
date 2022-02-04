eb60b3e50fb42916d63996c9d697625e94ca78a2
Sangharsha Chaulagain <sangharsha@fusemachines.com>
2022-02-04 18:27:03 +0545

    sign up user from ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :articles
   # Defines the root path route ("/")
   root "articles#index"
+
+  resources :articles
+
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
