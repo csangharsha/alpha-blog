cef153d25b7b87c0e3ac1a4a9254c7acc14d2d08
Sangharsha Chaulagain <sangharsha@fusemachines.com>
2022-02-07 13:21:52 +0545

    add admin requirement to add categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,21 +4,30 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@category = Category.create(name: "sports")
+		@user = User.create(username: "John", email: "john.doe@gmail.com", password: "password", admin: true)
 	end
 
 	test "should get categories index" do
 		get categories_path
-		assert :success
+		assert_response :success
 	end
 
 	test "show get categories new" do
+		sign_in_as(@user, "password")
 		get new_category_path
-		assert :success
+		assert_response :success
 	end
 
 	test "show get categories show" do
 		get category_path(@category)
-		assert :success
+		assert_response :success
 	end 
+
+	test "should redirect create when admin not logged in" do
+		assert_no_difference "Category.count" do
+			post categories_path, params: {category: {name: "sports"}}
+		end
+		assert_redirected_to categories_path
+	end
 
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,11 @@
 class CreateCategoriesTest < ActionDispatch::IntegrationTest 
 
-	test "get new category form and create category" do
+	def setup
+		@user = User.create(username: "John", email: "john.doe@gmail.com", password: "password", admin: true)
+	end
 
+	test "get new category form and create category" do
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template "categories/new"
 		assert_difference "Category.count", 1 do
@@ -14,7 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 	end
 
 	test "invalid cateogry submission results in failure" do
-
+		sign_in_as(@user, "password")
 		get new_category_path
 		assert_template "categories/new"
 		assert_no_difference "Category.count" do
